5373673927295ac6c2109097e058885fe28019a5
Mark Naughton <mark@marknaughton.com>
2022-10-10 15:55:28 +0100

    Add axes limits

--- src/main.py
+++ src/main.py
@@ -1,14 +1,29 @@
 import numpy as np
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
+
+
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 def main():
-    fig = plt.figure()
+    plt.rcParams['toolbar'] = 'None'
+    fig = plt.figure(num='3D Political Axis', figsize=(10,10))
+
+#    plt.axes.set_xlim(10)
+#    fig.set_ylim3d(10)
+#    fig.set_zlim3d(10)
+
+
     ax = plt.axes(projection ='3d')
 
     cords = get_cords()
 
     party_cords = get_party_cords()
+
+    ax.set_xlim(0, 10)
+    ax.set_ylim(0, 10)
+    ax.set_zlim(0, 10)
 
     ax.plot3D(cords[0], cords[1], cords[2], 'blue')
     ax.scatter3D(cords[0], cords[1], cords[2],  c=cords[2], cmap = 'cividis')
@@ -29,7 +44,7 @@ def plot(cords):
     
 def get_cords():
     x = input("Economic (X):")
-    y = input("GALTAN (Y):")
+    y = input("AL (Y):")
     z = input("Social (Z):")
 
     x = int(x)
